e2b459e413a54ea7021b4a17b30c35bc032a931a
0xClear <bahadoriborz@gmail.com>
2024-12-02 17:52:16 +0100

    getting data from  db

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,59 @@ import styles from "./page.module.css";
 import axios from "axios";
 import { useState } from "react";
 
+//table function 
+const Table=({theader, data, columns})=>{
+  //const {theader, data, columns}= props
+  return <div>
+    {/*
+    table component 
+    */}
+    <table className="table striped bordered">
+      <thead scope="col">
+        <tr>
+          {
+            theader.map((hValue, hIndex)=>{
+              return <th key={hIndex}>{hValue}</th>
+            })
+          }
+        </tr>
+      </thead>
+      <tbody>
+        {
+          //data > col 
+          //________________
+          data.map(
+            (tData, tIndex)=>{
+              return <tr key={tIndex}>
+                {
+                  columns.map((colValue, colIndex)=>{
+                    return <td key={colIndex}>{tData[colValue]} </td>
+                  })
+                }
+              </tr>
+            }
+          )
+        }
+      </tbody>
+    </table>
+  </div>
+}
+
 export default function Home() {
-  const [result, setResult] = useState('')
   //image state 
   const [images, setImages] = useState('');
   const [files, setfile] = useState(); 
 
-  const getName= async()=>{
-    const res = await axios.post('api/auth')
-    setResult(res?.data?.name)
-  }
+  // const getName= async()=>{
+  //   const res = await axios.post('api/auth')
+  //   setResult(res?.data?.name)
+  // }
+  const [users, setUsers] = useState([]);
 
   const getInfo= async()=>{
     const res = await axios.get('api/auth')
-    console.log("data", res)
+    setUsers(res?.data)
+    console.log("data",users)
   }
 
   //save image
@@ -52,12 +91,12 @@ export default function Home() {
     console.log("uploaded file ",)
 
   }
-  return (<div>
-    <button onClick={getName}>Get Name </button>
-    <h1>users:  {result}</h1>
+  return (<div className="container pt-4">
+    {/* <button className="btn btn-outline-primary" onClick={getName}>Get Name </button> */}
+
 
     <div>
-      <button onClick={getInfo}>get user Info</button>
+      <button onClick={getInfo} className="btn btn-outline-danger" >get user Info</button>
     </div>
     <div>
       <div>
@@ -66,6 +105,18 @@ export default function Home() {
       </div>
       <button onClick={saveImage} >Save Image</button>
     </div>
+
+
+    {/*
+    
+    **
+    user Info 
+    */}
+      <Table
+        theader={["Name", "Role", "Location", "Hash ID"]}
+        data={users} // Assuming 'users' is an array of user objects
+        columns={["name", "role", "location", "hashId"]} // Specify the column names based on the data structure
+      />
   </div>
   );
 }
