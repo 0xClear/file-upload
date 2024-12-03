3da55293a81c985599f923d498e8ff187a561ab1
0xClear <bahadoriborz@gmail.com>
2024-12-03 16:34:09 +0100

    file upload tailwind css

---
+++ src/component/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Modal = ({open, onClose, children}) => {
+  return (
+    <div onClick={onclose} className={`
+        fixed 
+        `} >
+        Modal component
+    </div>
+  )
+}
+
+export default Modal
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind scans all files in src/ directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
